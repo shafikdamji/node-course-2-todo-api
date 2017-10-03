70ebc3ad1ecf422cf1e7b98433493277069f45fd
Shafik <shafik_damji@yahoo.ca>
2017-10-03 09:41:32 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -30,6 +32,26 @@ app.get('/todos',(req, res)=>{
 	   res.status(400).send(e);
 	});
 })
+
+// GET //todo/234567
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+	
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+			// return;
+		};
+		
+		res.send({todo});
+	}).catch((e) => res.status(400).send()); 
+	
+});
+
 
 app.listen(3000, ()=>
 {
