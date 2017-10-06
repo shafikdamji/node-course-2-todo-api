f845a49d3ab6b8c70bfb95d59b5f30b9b00b076c
Shafik <shafik_damji@yahoo.ca>
2017-10-06 12:36:33 -0400

    Test the DELETE /todoss/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -105,6 +105,45 @@ describe ('GET /todos/:id', () => {
 	});
 	
 	
+});
+
+describe('DELETE /todos/:id', ()=> {
+	it('should remove a todo', (done) => {
+	   var hexID = todos[1]._id.toHexString();
+
+		request(app)
+		   .delete(`/todos/${hexID}`)
+		   .expect(200)
+		   .expect((res) => {
+			   expect(res.body.todo._id).toBe(hexID);
+		   })
+		   .end((err,res) => {
+			  if (err) {
+				  return done(err);
+			  } 
+			  
+			  Todo.findById(hexID).then((todo) => {
+				  expect(todo).toNotExist();
+				  done();
+			  }).catch((e) => done(e));
+			  
+		   });
+	});
 	
+ 	it('should return 404 if todo not found',(done) => {
+		request(app)
+		   .delete(`/todos/${new ObjectID().toHexString()}`)
+		   .expect(404)
+		   .end(done);
+		
+	});
+	 
+	if('should return 404 if object id is invalid', (done) => {
+		request(app)
+		   .delete(`/todos/123abc`)
+		   .expect(404)
+		   .end(done);
+	}); 
+
 	
 });
