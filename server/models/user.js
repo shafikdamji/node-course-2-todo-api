d7c0445990200b7e215dd54d26afe0e368cae064
Shafik <shafik_damji@yahoo.ca>
2017-10-19 14:19:09 -0400

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,40 @@ UserSchema.methods.generateAuthToken = function () {
 	
 };
 
+UserSchema.statics.findByToken = function (token) {
+
+	var User = this;
+	var decoded;
+	
+	try {
+		decoded = jwt.verify(token, 'abc123');
+	} catch (e) {
+	/*	return new Promise((resolve, reject) => {
+			reject();
+		});
+		*/
+		
+		return Promise.reject();
+		
+		
+	}
+
+	console.log('I am here');
+	console.log(decoded._id);
+	console.log(token);
+	
+	return User.findOne({
+		'_id': decoded._id,
+		"tokens.token" : token,
+		"tokens.access" : 'auth'
+	});
+	
+	
+	
+	
+};
+
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
